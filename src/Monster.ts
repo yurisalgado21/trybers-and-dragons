e75823fa37afd541d56538636faad02f86c94652
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2024-01-26 16:07:11 -0300

    requirement 09 done

--- src/Monster.ts
+++ src/Monster.ts
@@ -18,10 +18,10 @@ export default class Monster implements SimpleFighter {
   }
 
   receiveDamage(attackPoints: number): number {
-    if (this.lifePoints === 0) {
-      this._lifePoints -= 1;
-    }
     this._lifePoints -= attackPoints;
+    if (this._lifePoints === 0) {
+      this._lifePoints = -1;
+    }
     return this._lifePoints;
   }
 
