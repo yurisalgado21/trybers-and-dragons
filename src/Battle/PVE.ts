23e6b4c82824b034dfa5114b9e943591e3362547
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2024-01-27 02:21:23 -0300

    11 and 13 loading

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -5,15 +5,26 @@ import Battle from './Battle';
 
 export default class PVE extends Battle {
   private _player: Character;
-  private _monsters: Array<Monster | Fighter | SimpleFighter>;
+  private _monsters: Monster[] | Fighter[] | SimpleFighter[];
 
-  constructor(player: Character, monsters: Array<Monster | Fighter | SimpleFighter>) {
+  constructor(player: Character, monsters: Monster[] | Fighter[] | SimpleFighter[]) {
     super(player);
     this._player = player;
     this._monsters = monsters;
   }
 
+  private allMonstersDefeat(): boolean {
+    return this._monsters.every((monster) => monster.lifePoints === -1);
+  }
+
   override fight(): number {
+    this._monsters.forEach((opponent) => {
+      if (this._player.lifePoints !== -1 || !this.allMonstersDefeat()) {
+        this._player.attack(opponent);
+        opponent.attack(this._player);
+      }
+    });
+
     const resultBattle = super.fight();
     return resultBattle;
   }
---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import Battle, { PVE, PVP } from './Battle';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
+
+// Crie 3 objetos do tipo Character: 
+const player1: Character = new Character('julia');
+const player2: Character = new Character('Eliza');
+const player3: Character = new Character('Ana');
+
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+
+// Crie 2 objetos do tipo Monster: 
+const monster1: Monster = new Monster();
+const monster2: Dragon = new Dragon();
+
+// Crie um objeto do tipo PVP:
+const pvp: PVP = new PVP(player2, player3);
+
+// Crie um objeto do tipo PVE: 
+const pve: PVE = new PVE(player1, [monster1, monster2]);
+
+// Crie uma função chamada runBattles:
+function runBattles(battles: Battle[]) {
+  battles.forEach((battle) => {
+    battle.fight();
+  });
+}
+
+export {
+  player1,
+  player2,
+  player3,
+  monster1,
+  monster2,
+  pvp,
+  pve,
+  runBattles,
+};
